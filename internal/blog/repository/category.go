d9c0ff66a16f65e3445b5252ce69572a3fcae430
liguangliang <13537223157@163.com>
2024-01-03 17:11:43 +0800

    feat:add category repository

--- internal/blog/repository/category.go
+++ internal/blog/repository/category.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"errors"
 	"github.com/liang21/blog/internal/blog/biz"
 	"xorm.io/xorm"
 )
@@ -11,28 +12,57 @@ type categoryRepo struct {
 }
 
 func (c *categoryRepo) ListCategory(ctx context.Context) ([]*biz.Category, error) {
-	//TODO implement me
-	panic("implement me")
+	categorys := make([]*biz.Category, 0)
+	err := c.db.Find(&categorys)
+	if err != nil {
+		return nil, err
+	}
+	return categorys, nil
 }
 
 func (c *categoryRepo) GetCategory(ctx context.Context, id int64) (*biz.Category, error) {
-	//TODO implement me
-	panic("implement me")
+	category := &biz.Category{Id: id}
+	ok, err := c.db.Get(category)
+	if err != nil {
+		return nil, err
+	}
+	if !ok {
+		return nil, errors.New("category not found")
+	}
+	return category, nil
 }
 
 func (c *categoryRepo) CreateCategory(ctx context.Context, category *biz.Category) error {
-	//TODO implement me
-	panic("implement me")
+	result, err := c.db.Insert(category)
+	if err != nil {
+		return err
+	}
+	if result == 0 {
+		return errors.New("insert failed")
+	}
+	return nil
 }
 
 func (c *categoryRepo) UpdateCategory(ctx context.Context, id int64, category *biz.Category) error {
-	//TODO implement me
-	panic("implement me")
+	result, err := c.db.ID(id).Update(category)
+	if err != nil {
+		return err
+	}
+	if result == 0 {
+		return errors.New("update failed")
+	}
+	return nil
 }
 
 func (c *categoryRepo) DeleteCategory(ctx context.Context, id int64) error {
-	//TODO implement me
-	panic("implement me")
+	result, err := c.db.ID(id).Delete(&biz.Category{})
+	if err != nil {
+		return err
+	}
+	if result == 0 {
+		return errors.New("delete failed")
+	}
+	return nil
 }
 
 func NewCategoryRepo(db *xorm.Engine) biz.CategoryRepo {
